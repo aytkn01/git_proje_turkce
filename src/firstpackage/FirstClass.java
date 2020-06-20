48fe1b22c66a8d81a1b50d11975322c98b9409e3
unknown <hmksa0106@gmail.com>
2020-06-20 21:28:06 +0200

    comflict olusturma

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,8 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
-		System.out.println("third commit from local repository");
-               System.out.println("third commit from remote repository");
+		System.out.println("ucuncu commitimiz  local repository'den");
+        System.out.println("third commit from remote repository");
 	}
 
 }
